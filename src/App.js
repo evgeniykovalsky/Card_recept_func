629959035f30da596ad15abe675be002d703b4a9
Evgeniy <evgeniykovalsky04386@gmail.com>
2022-08-12 21:44:29 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useState } from 'react';
+
+
+
+export default function App (props) {
+    
+
+    const[description,setDescription]= useState(false)
+
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+
+       <div className="conteiner">
+    <div className="head">{props.data.name}</div>
+
+    <div className="mainBody">
+        <div className="picture"> 
+        <img src={props.data.image3} alt={props.data.id}/>
+        </div>
+        <div className="description">
+            <p><span>Назва страви :</span> {props.data.name}</p>
+            <p><span>Інгредієнти для приготування:</span> </p>
+           <p> <ul>
+                <li>{props.data.ingredient1}</li>
+                <li>{props.data.ingredient2}</li>
+                <li>{props.data.ingredient3}</li>
+                <li>{props.data.ingredient4}</li>
+                <li>{props.data.ingredient5}</li>
+                <li>{props.data.ingredient6}</li>
+            </ul></p>
+            <p className="lsDesc"><span>Опис страви:</span>{props.data.description}</p>
+                   </div>
     </div>
+    <div className="footer">
+        
+        <button 
+            onClick={()=>setDescription(prev =>!prev)}
+            >{description? 'Cкрыть':'Подробнее..'}</button>
+            {description && <p>{props.data.allDescription}</p>}
+    </div>
+    
+ </div>
+   
   );
+
 }
 
-export default App;
+
